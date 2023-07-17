66659cf363fba5fceb076d6e154802ea6a3e38c5
Anas-n7 <anasvvr2002@gamil.com>
2023-07-17 10:39:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // dislaay alert
+      showAlert(true, "danger", "please add text");
+    } else if (name && isEditing) {
+      //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "Edited succussfully");
+    } else {
+      showAlert(true, "success", "Added succussfully");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "No items are available");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item deleted succussfully");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="Type here..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && ( //listil onnum illenkil clear items kanathirikkan vendi
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            Clear Items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
